2e53b43f900e7a92c9183c66920a6ea60d734429
elwood-berry <eberry@anterasoftware.com>
2021-07-03 08:09:19 -0500

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'password-generator',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent {}
